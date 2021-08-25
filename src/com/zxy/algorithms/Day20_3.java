e510d619ff58daa36de9e3806eac1d41cf5dea09
zxy816 <zxy1988816@163.com>
2021-08-25 16:27:57 +0800

    Revert "."
    This reverts commit 35125aa762318feae049f82795364084d6b92a54.

--- src/com/zxy/algorithms/Day20_3.java
+++ src/com/zxy/algorithms/Day20_3.java
@@ -1,7 +1,31 @@
 package com.zxy.algorithms;
 
+import java.util.Arrays;
+
 public class Day20_3 {
     public static void main(String[] args) {
 
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+
+    /**
+     * 移动零
+     * 输入: [0,1,0,3,12]
+     * 输出: [1,3,12,0,0]
+     */
+    public static void moveZeroes(int[] nums) {
+        int l = 0, r = 0;
+        while (r < nums.length) {
+            if (nums[r] != 0) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+            }
+            r++;
+        }
     }
 }
--- src/com/zxy/effic/Section2.java
+++ src/com/zxy/effic/Section2.java
@@ -1,6 +1,111 @@
 package com.zxy.effic;
 
-public class Section2 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class Section2 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> listList = threeSum(nums);
+        for (int i = 0; i < listList.size(); i++) {
+            System.out.println(Arrays.toString(listList.get(i).toArray()));
+        }
+        System.out.println("--------------------四数之和-----------------------");
+        int[] nums4 = new int[]{2, 2, 2, 2, 2};
+        List<List<Integer>> list = fourSum(nums4, 8);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(Arrays.toString(list.get(i).toArray()));
+        }
+    }
+
+    /**
+     * 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c
+     * 使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组
+     * 输入：nums = [-1,0,1,2,-1,-4]
+     * 输出：[[-1,-1,2],[-1,0,1]]
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> listList = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return listList;
+        }
+        //[-4, -1, -1, 0, 1, 2]
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    listList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return listList;
+    }
+
+    /**
+     * 输入：nums = [1,0,-1,0,-2,2], target = 0
+     * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+     */
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> listList = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+            return listList;
+        }
+        //[-2, -1, 0, 0, 1, 2]
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+            if (nums[i] + nums[nums.length - 3] + nums[nums.length - 2] + nums[nums.length - 1] < target) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break;
+                }
+                if (nums[i] + nums[j] + nums[nums.length - 1] + nums[nums.length - 2] < target) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        listList.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        return listList;
+    }
 
 }
