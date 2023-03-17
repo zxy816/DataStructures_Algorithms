2b4c75fb6c95ae04856d8d165f3b6a612f564785
zxy816 <zxy1988816@163.com>
2023-03-17 17:25:10 +0800

    git commit -m .

--- src/com/zxy/leetCode/lc004.java
+++ src/com/zxy/leetCode/lc004.java
@@ -5,7 +5,139 @@ import java.util.*;
 
 public class lc004 {
     public static void main(String[] args) {
-        lc46();
+        lc547();
+    }
+
+    private static void lc547() {
+        int[][] isConnected = new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        int cities = isConnected.length;
+        boolean[] visited = new boolean[cities];
+        int provinces = 0;
+        for (int i = 0; i < cities; i++) {
+            if (!visited[i]) {
+                dfsCity(isConnected, visited, cities, i);
+                provinces++;
+            }
+        }
+        System.out.println("provinces:" + provinces);
+    }
+
+    private static void dfsCity(int[][] isConnected, boolean[] visited, int cities, int i) {
+        for (int j = 0; j < cities; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfsCity(isConnected, visited, cities, j);
+            }
+        }
+    }
+
+    /**
+     * [[1,2,2,1],[3,1,2],[1,3,2],[2,4],[3,1,2],[1,3,1,1]]
+     */
+    private static void lc554() {
+        List<List<Integer>> wall = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (List<Integer> line : wall) {
+            int n = line.size();
+            int sum = 0;
+            for (int i = 0; i < n - 1; i++) {
+                sum += line.get(i);
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
+            }
+        }
+        int maxMap = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            maxMap = Math.max(maxMap, entry.getValue());
+        }
+        System.out.println(wall.size() - maxMap);
+    }
+
+    private static void lc179() {
+        int[] nums = new int[]{3, 30, 34, 5, 9};
+        String[] strArr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strArr[i] = String.valueOf(nums[i]);
+        }
+        System.out.println(Arrays.toString(strArr));
+        Arrays.sort(strArr, (x, y) -> (y + x).compareTo(x + y));
+        if (strArr[0].equals("0")) {
+            System.out.println("0");
+            return;
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < strArr.length; i++) {
+            sb.append(strArr[i]);
+        }
+        System.out.println(sb.toString());
+    }
+
+    /**
+     * 3[a]10[bc]
+     * 3[a2[c]]
+     */
+    private static void lc394() {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        StringBuffer sb = new StringBuffer();
+        Stack digit = new Stack();
+        Stack letter = new Stack();
+        int k = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '[') {
+                digit.push(k);
+                letter.push(sb.toString());
+                k = 0;
+                sb.setLength(0);
+            } else if (c == ']') {
+                int num = Integer.parseInt(String.valueOf(digit.pop()));
+                StringBuffer temp = new StringBuffer();
+                for (int j = 0; j < num; j++) {
+                    temp.append(sb);
+                }
+                String res = String.valueOf(letter.pop());
+                sb = new StringBuffer(res + temp);
+            } else if (Character.isDigit(c)) {
+                k = k * 10 + Integer.parseInt(c + "");
+                System.out.println("k=" + k);
+            } else {
+                sb.append(c);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void lc475() {
+        int ans = 0;
+        int[] houses = new int[]{1, 2, 3, 4, 5, 6};
+        int[] heaters = new int[]{1, 4};
+        Arrays.sort(heaters);
+        for (int house : houses) {
+            int i = binarySearch(heaters, house);
+            int j = i + 1;
+            int leftDistance = i < 0 ? Integer.MAX_VALUE : house - heaters[i];
+            int rightDistance = j >= heaters.length ? Integer.MAX_VALUE : heaters[j] - house;
+            int curDistance = Math.min(leftDistance, rightDistance);
+            System.out.println("i = " + i + ",j = " + j + ",curDistance = " + curDistance);
+            ans = Math.max(ans, curDistance);
+        }
+        System.out.println("--------------" + ans);
+    }
+
+    private static int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        if (nums[left] > target) {
+            return -1;
+        }
+        while (left < right) {
+            int mid = (right - left + 1) / 2 + left;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid;
+            }
+        }
+        return left;
     }
 
     public static void lc46() {
