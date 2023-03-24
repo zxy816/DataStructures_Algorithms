b5c67ffaf4f08776224fa89d1e696f276eea9d50
zxy816 <zxy1988816@163.com>
2023-03-24 10:51:58 +0800

    .

--- src/com/zxy/leetCode/lc005.java
+++ src/com/zxy/leetCode/lc005.java
@@ -12,11 +12,155 @@ public class lc005 {
 //        lc56();
 //        lc200();
 //        lc11();
-        lc799();
+//        lc799();
+//        lc316();
+//        lc392();
+//        lc221();
+//        lc62();
+//        lc146();
+        lc70();
+    }
+
+    private static void lc70() {
+        int n = 5;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n]);
+    }
+
+    private static void lc146() {
+        String[][] board = new String[][]{{"X", "X", "X", "X"}, {"X", "O", "O", "X"}, {"X", "X", "O", "X"}, {"X", "O", "X", "X"}};
+        System.out.println(Arrays.deepToString(board));
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isEdge = i == 0 || j == 0 || i == m - 1 || j == n - 1;
+                if (isEdge && board[i][j] == "O") {
+                    dfs146(board, i, j);
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == "O") {
+                    board[i][j] = "X";
+                }
+                if (board[i][j] == "#") {
+                    board[i][j] = "O";
+                }
+            }
+        }
+        System.out.println(Arrays.deepToString(board));
+    }
+
+    private static void dfs146(String[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == "X" || board[i][j] == "#") {
+            return;
+        }
+        board[i][j] = "#";
+        dfs146(board, i - 1, j);
+        dfs146(board, i + 1, j);
+        dfs146(board, i, j - 1);
+        dfs146(board, i, j + 1);
+    }
+
+    private static void lc62() {
+        int m = 3, n = 7;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        System.out.println(dp[m - 1][n - 1]);
+    }
+
+    private static void lc221() {
+        String[][] matrix = new String[][]{{"1", "0", "1", "0", "0"}, {"1", "0", "1", "1", "1"}, {"1", "1", "1", "1", "1"}, {"1", "0", "0", "1", "0"}};
+        int maxSide = 0;
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == "1") {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        System.out.println(maxSide * maxSide);
+    }
+
+    private static void lc392() {
+        String s = "abc", t = "ahbgdc";
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        System.out.println(i == s.length());
+    }
+
+    private static void lc316() {
+        String s = "cbacdcbc";
+        boolean[] used = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!used[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        used[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                used[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+
+        }
+        System.out.println(sb.toString());
     }
 
     private static void lc799() {
-
+        int poured = 2, query_row = 1, query_glass = 1;
+        double[] row = new double[]{poured};
+        for (int i = 1; i <= query_row; i++) {
+            double[] nextRow = new double[i + 1];
+            for (int j = 0; j < i; j++) {
+                double v = row[j];
+                if (v > 1) {
+                    nextRow[j] += (v - 1) / 2;
+                    nextRow[j + 1] += (v - 1) / 2;
+                }
+            }
+            row = nextRow;
+        }
+        System.out.println(Math.min(1, row[query_glass]));
     }
 
     private static void lc11() {
---
+++ src/com/zxy/leetCode/lc006.java
@@ -0,0 +1,11 @@
+package com.zxy.leetCode;
+
+public class lc006 {
+    public static void main(String[] args) {
+        lc17();
+    }
+
+    private static void lc17() {
+
+    }
+}
